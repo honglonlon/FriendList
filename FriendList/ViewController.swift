8435d8748c0abf23f331eccec8f8c80076456c73
Andre <andreyang@softmobile.com.tw>
2025-04-28 20:35:17 +0800

    Initial Commit

---
+++ FriendList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendList
+//
+//  Created by 楊豐榮 on 2025/4/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
